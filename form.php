b75adaacbe2b0eb0950cf75347834fb3dce8245c
ae.ergashev <ae.ergashev@gmail.com>
2022-06-19 14:50:49 -0700

    still trying that shit

--- form.php
+++ form.php
@@ -1,9 +1,22 @@
 <?php
-define ('https://www.grandsolutionsinc.com/',"https://api.telegram.org/bot5130221153:AAHxs162H6U677rA3fo7OuRFX7mdHKSY7DU/");
-$name = $_GET['name'];
-$message = $_GET['message'];
-$chat_id = '1851854218';
-$message = urlencode("Name:".$name."\n Message : ".$message);
-file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
+$name = $_POST['name'];
+$message = $_POST['message'];
+$token = "5130221153:AAHxs162H6U677rA3fo7OuRFX7mdHKSY7DU";
+$chat_id = '-798142020';
+$arr = array(
+    'Имя пользователя: ' => $name,
+    'Телефон: ' => $message,
+  );
 
+foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
+    &parse_mode=html&text={$txt}","r");
+
+  if ($sendToTelegram) {
+    echo ('success');
+  } else {
+    echo "Error";
+  }
 ?>
